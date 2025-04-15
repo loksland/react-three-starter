90d279b7b02fa5479d203d8c94d1827277809c71
Lachlan Nuttall <github@maker.com.au>
2025-04-15 12:17:41 +1000

    Base path typo fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-const publishSubDirPath = 'rect-three-starter/'; // Trail path with slash
+const publishSubDirPath = 'react-three-starter/'; // Trail path with slash
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
